bc8505876b7c5de91225a0c671487d0b5e8d3ee0
ecknight <ellyknight@gmail.com>
2022-01-20 16:59:33 -0700

    optimizing priors, remove if else statements

--- 07.DiscreteChoice.R
+++ 07.DiscreteChoice.R
@@ -30,7 +30,7 @@ ggplot(dat, aes(x=tree, y=used, colour=scale)) +
   facet_wrap(~Season, scales="free")
 
 ggplot(dat, aes(x=water, y=used, colour=Season)) + 
-  #  geom_point() +
+  geom_point() +
   geom_smooth() +
   facet_grid(Season~scale, scales="free")
 
@@ -44,6 +44,10 @@ ggplot(dat, aes(x=evi, y=used, colour=scale)) +
   geom_smooth() +
   facet_wrap(~Season, scales="free")
 
+ggplot(dat %>% dplyr::filter(Season=="Winter", scale=="pt", water > 0)) +
+  geom_histogram(aes(x=water)) +
+  facet_wrap(~used, scales="free")
+
 #2. Set up loop----
 loop <- dat %>% 
   dplyr::select(scale, Season) %>% 
@@ -56,8 +60,8 @@ summary.list <- list()
 betas.list <- list()
 fit.list <- list()
 
-for(i in 1:nrow(loop)){
-#for(i in 1:11){  
+#for(i in 1:nrow(loop)){
+for(i in 2:2){  
   
   #3. Subset data----
   scale.i <- loop$scale[i]
@@ -89,91 +93,29 @@ for(i in 1:nrow(loop)){
   X2 <- matrix(dat.i$tree.s, nrow=nsets, ncol=nchoices, byrow=TRUE)
   X3 <- matrix(dat.i$crops.s, nrow=nsets, ncol=nchoices, byrow=TRUE)
   X4 <- matrix(dat.i$water.s, nrow=nsets, ncol=nchoices, byrow=TRUE)
-  
-  if(scale.i=="land" & season.i!="Winter"){
-    X5 <- matrix(dat.i$water2.s, nrow=nsets, ncol=nchoices, byrow=TRUE)
-  }
 
   #max value in the bird vector above (i.e., an integer representing how many unique birds there are)
   nbirds <- max(bird)
   
   #5. Model specification----
   
-  if(scale.i=="land" & season.i!="Winter"){
-    sink("Mixed model.txt")
-    cat("model{    
+  sink("Mixed model.txt")
+  cat("model{    
 #Priors
 mu.beta1 ~ dnorm(0, 0.01)
-tau.beta1 ~ dgamma(10, 0.1)
+tau.beta1 ~ dgamma(7, 0.1)
 sigma.beta1 <- 1/sqrt(tau.beta1)
 
 mu.beta2 ~ dnorm(0, 0.01)
-tau.beta2 ~ dgamma(10, 0.1)
+tau.beta2 ~ dgamma(7, 0.1)
 sigma.beta2 <- 1/sqrt(tau.beta2)
 
 mu.beta3 ~ dnorm(0, 0.01)
-tau.beta3 ~ dgamma(10, 0.1)
+tau.beta3 ~ dgamma(7, 0.1)
 sigma.beta3 <- 1/sqrt(tau.beta3)
 
 mu.beta4 ~ dnorm(0, 0.01)
-tau.beta4 ~ dgamma(10, 0.1)
-sigma.beta4 <- 1/sqrt(tau.beta4)  
-
-mu.beta5 ~ dnorm(0, 0.01)
-tau.beta5 ~ dgamma(10, 0.1)
-sigma.beta5 <- 1/sqrt(tau.beta5)  
-
-for(b in 1:nbirds){    
-beta1[b] ~ dnorm(mu.beta1, tau.beta1)    
-beta2[b] ~ dnorm(mu.beta2, tau.beta2)    
-beta3[b] ~ dnorm(mu.beta3, tau.beta3)    
-beta4[b] ~ dnorm(mu.beta4, tau.beta4)    
-beta5[b] ~ dnorm(mu.beta5, tau.beta5)   
-}    
-
-#Likelihood   
-    for(i in 1:nsets){    
-    y[i,1:nchoices[i]] ~ dmulti(p[i,1:nchoices[i]],1)    
-    ysim[i,1:nchoices[i]] ~ dmulti(p[i,1:nchoices[i]],1)    
-    
-    for(j in 1:nchoices[i]){    
-    log(phi[i,j]) <- beta1[bird[i]]*X1[i,j] + beta2[bird[i]]*X2[i,j]+ beta3[bird[i]]*X3[i,j]+ beta4[bird[i]]*X4[i,j] + beta5[bird[i]]*X5[i,j]
-    
-    p[i,j] <- phi[i,j]/(sum(phi[i,1:nchoices[i]]))    
-    
-    D1[i,j] <- pow(y[i,j] - p[i,j], 2)    
-    D1sim[i,j] <- pow(ysim[i,j] - p[i,j], 2)    
-    }    
-    
-    D2[i] <- sum(D1[i,1:nchoices[i]])    
-    D2sim[i] <- sum(D1sim[i,1:nchoices[i]])
-    }    
-    
-    fit.data <- sum(D2[1:nsets])    
-    fit.sim <- sum(D2sim[1:nsets])    
-    bpv <- fit.data - fit.sim    
-    }",fill = TRUE)
-    
-    sink()
-  }
-  else {
-    sink("Mixed model.txt")
-    cat("model{    
-#Priors
-mu.beta1 ~ dnorm(0, 0.01)
-tau.beta1 ~ dgamma(10, 0.1)
-sigma.beta1 <- 1/sqrt(tau.beta1)
-
-mu.beta2 ~ dnorm(0, 0.01)
-tau.beta2 ~ dgamma(10, 0.1)
-sigma.beta2 <- 1/sqrt(tau.beta2)
-
-mu.beta3 ~ dnorm(0, 0.01)
-tau.beta3 ~ dgamma(10, 0.1)
-sigma.beta3 <- 1/sqrt(tau.beta3)
-
-mu.beta4 ~ dnorm(0, 0.01)
-tau.beta4 ~ dgamma(10, 0.1)
+tau.beta4 ~ dgamma(7, 0.1)
 sigma.beta4 <- 1/sqrt(tau.beta4)  
 
 for(b in 1:nbirds){    
@@ -205,72 +147,28 @@ beta4[b] ~ dnorm(mu.beta4, tau.beta4)
     fit.sim <- sum(D2sim[1:nsets])    
     bpv <- fit.data - fit.sim    
     }",fill = TRUE)
-    
-    sink()
-  }
   
+  sink()
+
   #6. JAGS parameters----
   #Specify data for JAGS
-  if(scale.i=="land" & season.i!="Winter"){
-    win.data = list(y=y, nsets=nsets,  nchoices=nchoices, bird=bird, X1=X1, X2=X2, X3=X3, X4=X4, X5=X5, nbirds=nbirds)
-  }
-  else{
-    win.data = list(y=y, nsets=nsets,  nchoices=nchoices, bird=bird, X1=X1, X2=X2, X3=X3, X4=X4, nbirds=nbirds)
-  }
+  win.data = list(y=y, nsets=nsets,  nchoices=nchoices, bird=bird, X1=X1, X2=X2, X3=X3, X4=X4, nbirds=nbirds)
   
   #Specify initial values
   inits = function()list(mu.beta1=rnorm(1), tau.beta1=runif(1))
   
   #Specify parameters to track
-  if(scale.i=="land" & season.i!="Winter"){
-    params = c("mu.beta1", "sigma.beta1", "beta1",  
-               "mu.beta2", "sigma.beta2", "beta2",           
-               "mu.beta3", "sigma.beta3", "beta3",           
-               "mu.beta4", "sigma.beta4", "beta4", 
-               "mu.beta5", "sigma.beta5", "beta5", 
-               "p", "fit.data", "fit.sim", "bpv")
-  }
-  else{
-    params = c("mu.beta1", "sigma.beta1", "beta1",  
-               "mu.beta2", "sigma.beta2", "beta2",           
-               "mu.beta3", "sigma.beta3", "beta3",           
-               "mu.beta4", "sigma.beta4", "beta4", 
-               "p", "fit.data", "fit.sim", "bpv")
-  }
+  params = c("mu.beta1", "sigma.beta1", "beta1",  
+             "mu.beta2", "sigma.beta2", "beta2",           
+             "mu.beta3", "sigma.beta3", "beta3",           
+             "mu.beta4", "sigma.beta4", "beta4", 
+             "p", "fit.data", "fit.sim", "bpv")
 
-  
   #Number of chains, iterations, burnin, and thinning 
-  nc=3; ni=200000; nb=100000; nt=50; na=1000 
+  nc=3; ni=300000; nb=100000; nt=50; na=1000 
   
   #7. Run JAGS----
   outM = jags(win.data, inits, params, "Mixed model.txt", n.chains=nc, n.thin=nt, n.iter=ni, n.burnin=nb, parallel=T, n.adapt = 1000)
-  
-  #8. Predictions----
-#  outmat <- as.matrix(outM)
-  
-#  newdat.tree <- expand.grid(X1=seq(min(dat.i$tree.s), max(dat.i$tree.s), 0.1),
-#                         X2=mean(dat.i$crops.s),
-#                         X3=mean(dat.i$bare.s),
-#                         X4=mean(dat.i$water.s),
-#                         X5=mean(dat.i$water2.s))
-  
-#  newdat.crops <- expand.grid(X1=mean(dat.i$tree.s),
-#                         X2=seq(min(dat.i$crops.s), max(dat.i$crops.s), 0.1),
-#                         X3=mean(dat.i$bare.s),
-#                         X4=mean(dat.i$water.s),
-#                         X5=mean(dat.i$water2.s))
-  
-#  newdat.bare <- expand.grid(X1=mean(dat.i$tree.s),
-#                         X2=mean(dat.i$bare.s),
-#                         X3=seq(min(dat.i$bare.s), max(dat.i$bare.s), 0.1),
-#                         X4=mean(dat.i$water.s),
-#                         X5=mean(dat.i$water2.s))
-  
-#  newdat.water <- expand.grid(X1=mean(dat.i$tree.s),
-#                             X2=mean(dat.i$bare.s),
-#                             X3=mean(dat.i$water.s),
-#                             X4=seq(min(dat.i$water.s), max(dat.i$water.s), 0.1),
-#                             X5=X4^2)
   
   #9. Save outputs----
   model.list[[i]] <- data.frame(DIC = outM$DIC,
@@ -284,25 +182,12 @@ beta4[b] ~ dnorm(mu.beta4, tau.beta4)
            season=season.i)
   summary.list[[i]]$val <- row.names(outM$summary)
   
-  if(scale.i=="land" & season.i!="Winter"){
-    betas.list[[i]] <- data.frame(beta1=outM$sims.list$mu.beta1,
-                                  beta2=outM$sims.list$mu.beta2,
-                                  beta3=outM$sims.list$mu.beta3,
-                                  beta4=outM$sims.list$mu.beta4,
-                                  beta5=outM$sims.list$mu.beta5,
-                                  scale=scale.i,
-                                  season=season.i)
-  }
-  else{
-    betas.list[[i]] <- data.frame(beta1=outM$sims.list$mu.beta1,
-                                  beta2=outM$sims.list$mu.beta2,
-                                  beta3=outM$sims.list$mu.beta3,
-                                  beta4=outM$sims.list$mu.beta4,
-                                  beta5=NA,
-                                  scale=scale.i,
-                                  season=season.i)
-  }
-
+  betas.list[[i]] <- data.frame(beta1=outM$sims.list$mu.beta1,
+                                beta2=outM$sims.list$mu.beta2,
+                                beta3=outM$sims.list$mu.beta3,
+                                beta4=outM$sims.list$mu.beta4,
+                                scale=scale.i,
+                                season=season.i)
   
   fit.list[[i]] <- data.frame(fit.data=outM$sims.list$fit.data,
                          fit.sim=outM$sims.list$fit.sim,
@@ -319,16 +204,21 @@ beta4[b] ~ dnorm(mu.beta4, tau.beta4)
 #10. Collapse outputs----
 model <- rbindlist(model.list)
 summary <- rbindlist(summary.list)
-betas <- rbindlist(betas.list) %>% 
-  pivot_longer(beta1:beta5, names_to="beta", values_to="value")
+betas <- rbindlist(betas.list, fill=TRUE) %>% 
+  dplyr::select(-beta5) %>% 
+  pivot_longer(beta1:beta4, names_to="beta", values_to="value") 
 fit <- rbindlist(fit.list)
 
+#11. Save workspace----
+save.image("CONIRoosting_WorkSpace.Rdata")
+#load("CONIRoosting_WorkSpace.Rdata")
+
 #11. Inspect traceplots
-outM <- readRDS("Models/DiscreteChoice_pt_SpringMig.RDS")
-#summary(outM)
+outM <- readRDS("Models/DiscreteChoice_pt_FallMig.RDS")
+summary(outM)
 jagsUI::traceplot(outM, parameters = c("mu.beta1", "mu.beta2", "mu.beta3", "mu.beta4", "mu.beta5"))
 jagsUI::traceplot(outM, parameters = c("sigma.beta1", "sigma.beta2", "sigma.beta3", "sigma.beta4", "sigma.beta5"))
-#non-convergence: landscape winter (REs only), landscape fall - evi sigma, landscape breed (REs only), hr breed (REs only), hr winter (REs only), pt winter (REs only), pt breed (REs only)
+#fall.pt.water, breed.hr.crop, winter.pt.water - R=1.239, winter.hr.crop
 
 #12. Beta overlap----
 overlap <- summary %>% 
@@ -336,8 +226,7 @@ overlap <- summary %>%
   mutate(cov = case_when(val=="mu.beta1" ~ "evi",
                          val=="mu.beta2" ~ "tree",
                          val=="mu.beta3" ~ "crop",
-                         val=="mu.beta4" ~ "water",
-                         val=="mu.beta5" ~ "water2"))
+                         val=="mu.beta4" ~ "water"))
 
 overlap$scale <- factor(overlap$scale, levels=c("pt", "hr"))
 
@@ -355,13 +244,20 @@ table(overlap.0$season, overlap.0$scale)
 betas$cov <- case_when(betas$beta=="beta1" ~ "evi",
                        betas$beta=="beta2" ~ "tree",
                        betas$beta=="beta3" ~ "crop",
-                       betas$beta=="beta4" ~ "water",
-                       betas$beta=="beta5" ~ "water2")
+                       betas$beta=="beta4" ~ "water")
 
-betas$scale <- factor(betas$scale, levels=c("pt", "hr", "land"))
+betas$scale <- factor(betas$scale, levels=c("pt", "hr"))
 
 betas.0 <- betas %>% 
   right_join(overlap.0)
+
+ggplot(betas) +
+  geom_density(aes(x=value, colour=season)) +
+  geom_density(data=betas.0, aes(x=value, fill=season, colour=season), alpha=0.5) +
+  geom_vline(aes(xintercept=0)) +
+  facet_grid(scale ~ cov, scales="free")
+
+ggsave(filename="Figures/Betas.jpeg", width=14, height=8)
 
 ggplot(betas.0 %>% 
          dplyr::filter(cov %in% c("evi", "crop", "tree", "water"))) +
@@ -377,8 +273,7 @@ ggplot(betas %>%
   geom_vline(aes(xintercept=0)) +
   facet_grid(scale ~ season, scales="free")
 
-save.image("CONIRoosting_WorkSpace2.Rdata")
-#load("CONIRoosting_WorkSpace.Rdata")
+
 
 #14. Sum of selection strength----
 betas.sum <- betas.0 %>% 
